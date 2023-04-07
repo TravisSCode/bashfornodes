507e211afe63d0d63f4253cec95d89042bf48ce8
TravisSCode <74230457+TravisSCode@users.noreply.github.com>
2023-04-07 17:32:05 +0300

    Update lamina1.sh

--- lamina1.sh
+++ lamina1.sh
@@ -1,14 +1,18 @@
 sudo apt update && sudo apt upgrade --yes
 sleep 5
+
 sudo apt-get install tmux
 sleep 5
+
 wget https://lamina1.github.io/lamina1/lamina1.latest.ubuntu-latest.tar.gz
 sleep 5
+
 tar -xvzf lamina1.latest.ubuntu-latest.tar.gz
 sleep 10
-cd lamina1
+
+cd lamina1/
 sleep 5
+
 curl https://lamina1.github.io/lamina1/config.testnet.tar | tar xf - 
 
-echo "Копируем ID и смотрим статус синхронизации"
-rm -rf lamina1.latest.ubuntu-latest.tar.gz
+rm -rf $HOME/root/lamina1.latest.ubuntu-latest.tar.gz
