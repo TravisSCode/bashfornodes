76cfa35d11757acb33aec8de897c89b7199052bf
TravisSCode <74230457+TravisSCode@users.noreply.github.com>
2023-04-07 17:49:41 +0300

    Update lamina1.sh

--- lamina1.sh
+++ lamina1.sh
@@ -1,5 +1,12 @@
 sudo apt update && sudo apt upgrade --yes
 sleep 5
+sudo apt install ufw
+sudo ufw disable
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow ssh
+sudo ufw allow in 9650
+sudo ufw allow in 9671
 
 sudo apt-get install tmux
 sleep 5
@@ -14,3 +21,4 @@ cd lamina1/
 sleep 5
 
 rm -rf $HOME/root/lamina1.latest.ubuntu-latest.tar.gz
+sudo ufw enable
