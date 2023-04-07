40654490751d93d96fc03b29efc68e4e78f79a8d
TravisSCode <74230457+TravisSCode@users.noreply.github.com>
2023-04-07 17:02:16 +0300

    Update lamina1.sh

--- lamina1.sh
+++ lamina1.sh
@@ -6,8 +6,10 @@ https://lamina1.github.io/lamina1/lamina1.latest.ubuntu-latest.tar.gz
 
 tar -xvzf lamina1.latest.ubuntu-latest.tar.gz
 
-rm -rf lamina1.latest.ubuntu-latest.tar.gz
-
 cd lamina1
 
 curl https://lamina1.github.io/lamina1/config.testnet.tar | tar xf - 
+
+echo "Копируем ID и смотрим статус синхронизации"
+
+rm -rf lamina1.latest.ubuntu-latest.tar.gz
