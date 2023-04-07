1b179458b76ec80e1de33be7d2b56786a3793d19
TravisSCode <74230457+TravisSCode@users.noreply.github.com>
2023-04-07 16:59:05 +0300

    Create lamina1.sh

---
+++ lamina1.sh
@@ -0,0 +1,13 @@
+sudo apt update && sudo apt upgrade --yes
+
+sudo apt-get install tmux
+
+https://lamina1.github.io/lamina1/lamina1.latest.ubuntu-latest.tar.gz
+
+tar -xvzf lamina1.latest.ubuntu-latest.tar.gz
+
+rm -rf lamina1.latest.ubuntu-latest.tar.gz
+
+cd lamina1
+
+curl https://lamina1.github.io/lamina1/config.testnet.tar | tar xf - 
